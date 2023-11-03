d2f72b7e8ae3d38f5c6380e67fc289f7f4610a51
HDialani <45036669+HDialani@users.noreply.github.com>
2023-11-03 10:59:11 +0100

    Delete script.js
    No longer needed

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for handling project dropdown
-function navigateToProject() {
-    let dropdown = document.getElementById("project");
-    let selectedProject = dropdown.options[dropdown.selectedIndex].value;
-
-    if (selectedProject) {
-        window.location.href = selectedProject;
-    }
-}
